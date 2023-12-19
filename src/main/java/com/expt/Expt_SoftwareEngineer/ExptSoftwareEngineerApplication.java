92c90bff6636a0b5f772f4e4b635b45914a1b3ec
Unrealfeathers <Unrealfeathers@outlook.com>
2023-12-19 20:28:05 +0800

    New

---
+++ src/main/java/com/expt/Expt_SoftwareEngineer/ExptSoftwareEngineerApplication.java
@@ -0,0 +1,13 @@
+package com.expt.Expt_SoftwareEngineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExptSoftwareEngineerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExptSoftwareEngineerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expt/Expt_SoftwareEngineer/ExptSoftwareEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expt.Expt_SoftwareEngineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExptSoftwareEngineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
