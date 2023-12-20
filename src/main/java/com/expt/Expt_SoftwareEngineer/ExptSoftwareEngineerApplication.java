d007614d433d3710807e9bec4bb5815c7fe655d2
Unrealfeathers <Unrealfeathers@outlook.com>
2023-12-20 14:52:27 +0800

    添加相关依赖，完善目录结构

--- src/main/java/com/expt/Expt_SoftwareEngineer/ExptSoftwareEngineerApplication.java
+++ src/main/java/com/expt/Expt_SoftwareEngineer/ExptSoftwareEngineerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExptSoftwareEngineerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ExptSoftwareEngineerApplication.class, args);
 	}
 
