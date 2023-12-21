8fec7aff35d4129f67547df1381096ea8789b06e
Unrealfeathers <Unrealfeathers@outlook.com>
2023-12-21 17:10:33 +0800

    实现登录认证，未测试

--- src/main/java/com/expt/Expt_SoftwareEngineer/controller/UserController.java
+++ src/main/java/com/expt/Expt_SoftwareEngineer/controller/UserController.java
@@ -3,6 +3,7 @@ package com.expt.Expt_SoftwareEngineer.controller;
 import com.expt.Expt_SoftwareEngineer.pojo.Result;
 import com.expt.Expt_SoftwareEngineer.pojo.User;
 import com.expt.Expt_SoftwareEngineer.service.UserService;
+import com.expt.Expt_SoftwareEngineer.utils.JwtUtil;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -10,36 +11,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/user")
 @Validated
 public class UserController {
     @Autowired
     private UserService userService;
+
     @PostMapping("/signup")
-    public Result signup(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$") String password){
+    public Result signup(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         // 查询用户名是否被占用
         User user = userService.findByUserName(username);
-        if (user == null){
+        if (user == null) {
             // 未被占用，注册用户
-            userService.register(username,password);
+            userService.register(username, password);
             return Result.success();
-        }else {
+        } else {
             // 被占用，返回错误
             return Result.error("用户名已被占用");
         }
     }
 
     @PostMapping("/login")
-    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$") String password){
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         // 根据用户名查询用户
         User loginUser = userService.findByUserName(username);
-        if (loginUser == null){
+        if (loginUser == null) {
             return Result.error("用户名错误");
         }
         if (password.equals(loginUser.getPassword())) {
-            // todo JWT
-            return Result.success("JWT令牌");
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("user_id", loginUser.getUserID());
+            claims.put("user_name", loginUser.getUserName());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
         }
         return Result.error("密码错误");
     }
---
+++ src/main/java/com/expt/Expt_SoftwareEngineer/utils/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.expt.Expt_SoftwareEngineer.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+
+public class JwtUtil {
+    private static final String KEY = "215256";  // 加密密钥值
+    // 生成Token并返回
+    public static String genToken(Map<String, Object> claims) {
+        return JWT.create()
+                .withClaim("claims", claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000*60*60*12))
+                .sign(Algorithm.HMAC256(KEY));
+    }
+    // 验证Token
+    public static Map<String, Object> parseToken(String token) {
+        return JWT.require(Algorithm.HMAC256(KEY))
+                .build()
+                .verify(token)
+                .getClaim("claims")
+                .asMap();
+    }
+}
